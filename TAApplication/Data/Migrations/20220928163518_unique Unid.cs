99ddc95a4af8f125002e50f1a5d5e9cb019bbf78
rj-davidson <hello@rjd.app>
2022-09-28 18:08:45 -0600

    Add unid migrations

---
+++ TAApplication/Data/Migrations/20220928163518_unique Unid.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TAApplication.Data.Migrations
+{
+    public partial class uniqueUnid : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_Unid",
+                table: "AspNetUsers",
+                column: "Unid",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_Unid",
+                table: "AspNetUsers");
+        }
+    }
+}
