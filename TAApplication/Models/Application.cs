dcb4a3b8a931f2f1c8d83dd02a659d097f9a69d1
rj-davidson <hello@rjd.app>
2022-10-04 16:52:01 -0600

    Near complete data annotations for Models

--- TAApplication/Models/Application.cs
+++ TAApplication/Models/Application.cs
@@ -14,56 +14,64 @@
 	This is the C# model for Applications
  */
 using System.ComponentModel.DataAnnotations;
+using TAApplication.Areas.Identity.Data;
 
 namespace TAApplication.Models
 {
+    public enum DegreeLevel { BS, MS, PhD }
+
     public class Application : ModificationTracking
     {
-        // TODO
-        [DisplayFormat(NullDisplayText = "", DataFormatString = "")] // TODO DateFormat or DateFormatString?
-        [DataType(DataType.DateTime)]
-        [StringLength(50000)]
-        [Url]
-        // END TODO
         [Required]
-        [Display(Name = "", ShortName = "", Prompt = "", Description = "")]
-        public Enum CurrentDegree { get; set; }
-        
+        public int ID { get; set; }
+
         [Required]
-        [Display(Name = "", ShortName = "", Prompt = "", Description = "")]
+        [Display(Name = "Pursuing Degree", ShortName = "Degree")]
+        public DegreeLevel PursuingDegree { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        [Display(Name = "Program enrolled in", ShortName = "Program", Prompt = "", Description = "Department and Program pursuing")]
         public String Program { get; set; }
-        
+
         [Required]
-        [Range(0.0,4.0)]
-        [Display(Name = "", ShortName = "", Prompt = "", Description = "")]
+        [Range(0.0, 5.0)]
+        [Display(Name = "Grade Point Average", ShortName = "GPA")]
         public double GPA { get; set; }
-        
+
         [Required]
-        [Range(5,20)]
-        [Display(Name = "", ShortName = "", Prompt = "", Description = "")]
+        [Range(5, 20)]
+        [Display(Name = "Hours Wanted", ShortName = "Hours", Prompt = "", Description = "Number of hours the applicant wants to work per week")]
         public int HoursWanted { get; set; }
-        
+
         [Required]
-        [Display(Name = "", ShortName = "", Prompt = "", Description = "")]
+        [Display(Name = "Early Availability", ShortName = "Can Start Early", Prompt = "", Description = "Is the applicant available for work a week before the class start date")]
         public bool EarlyAvailability { get; set; }
-        
+
         [Required]
-        [Range(0,999)]
-        [Display(Name = "", ShortName = "", Prompt = "", Description = "")]
+        [Range(0, 999)]
+        [Display(Name = "Semesters Completed at U of U", ShortName = "Semesters Completed", Prompt = "", Description = "Number of semesters the applicant has completed at the U of U")]
         public int SemestersCompletedAtUtah { get; set; }
-        
-        [Display(Name = "", ShortName = "", Prompt = "", Description = "")]
+
+        [StringLength(50000)]
+        [Display(Name = "Personal Statement", ShortName = "Statement", Prompt = "", Description = "Applicant's personal statement")]
         public String? PersonalStatement { get; set; }
-        
-        [Display(Name = "", ShortName = "", Prompt = "", Description = "")]
+
+        [StringLength(150)]
+        [Display(Name = "Transfer School", ShortName = "Transfered from", Prompt = "", Description = "Name of the school the applicant has transfered from")]
         public String? TransferSchool { get; set; }
-        
-        [Display(Name = "", ShortName = "", Prompt = "", Description = "")]
+
+        [Url]
+        [StringLength(300)]
+        [Display(Name = "LinkedIn Profile", ShortName = "LinkedIn", Prompt = "", Description = "Applicant's LinkedIn social media profile url")]
         public String? LinkedInURL { get; set; }
-        
-        [Display(Name = "", ShortName = "", Prompt = "", Description = "")]
+
+        [StringLength(300)]
+        [Display(Name = "Upload Resume", ShortName = "Resume", Prompt = "", Description = "Applicant's resume")]
         public String? ResumeFilename { get; set; }
-        //TODO: Creation Date
-        //TODO: Modification Date
+
+        // Navigation Properties
+        [Required]
+        public TAUser Applicant { get; set; } = null!;
     }
 }
--- TAApplication/Models/ModificationTracking.cs
+++ TAApplication/Models/ModificationTracking.cs
@@ -1,18 +1,33 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.VisualBasic;
+using System.ComponentModel.DataAnnotations;
 
 namespace TAApplication.Models
 {
     public class ModificationTracking
     {
+        [Required]
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Creation Date")]
+        //[DisplayFormat(DateFormat.GeneralDate.ToString("MM/dd/yyyy"))]
+        //[DisplayFormat(NullDisplayText = "", DataFormatString = "")] // TODO DateFormat or DateFormatString?
         [ScaffoldColumn(false)]
         public DateTime CreationDate {get; set; }
 
+        [Required]
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Modification Date")]
         [ScaffoldColumn(false)]
         public DateTime ModificationDate { get; set; }
 
+        [Required]
+        [StringLength(150)]
+        [Display(Name = "Created by")]
         [ScaffoldColumn(false)]
         public String CreatedBy { get; set; }
 
+        [Required]
+        [StringLength(150)]
+        [Display(Name = "Modified by")]
         [ScaffoldColumn(false)]
         public String ModifiedBy { get; set; }
     }
