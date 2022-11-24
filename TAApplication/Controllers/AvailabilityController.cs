22bae49185f196e4690a4cdd4b0f31d46372e74f
rj-davidson <hello@rjd.app>
2022-11-23 22:03:48 -0700

    Slot Controller Created

---
+++ TAApplication/Controllers/AvailabilityController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TAApplication.Areas.Identity.Data;
+using TAApplication.Data;
+using TAApplication.Models;
+
+namespace TAApplication.Controllers
+{
+    [Authorize]
+    public class AvailabilityController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<TAUser> _um;
+
+        public AvailabilityController(ApplicationDbContext db, UserManager<TAUser> um)
+        {
+            _db = db;
+            _um = um;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Replaces the existing slots for the current user
+        /// </summary>
+        /// <param name="newSlots">Complete set of slots for a TA schedule</param>
+        public async void SetSchedule(Slot[] newSlots)
+        {
+            // Gets and deletes all previous slots
+            TAUser currUser = await _um.GetUserAsync(User);
+            var oldSlots = _db.Slots.Where(slot => slot.TAUserId == currUser.Id);
+            foreach (var slot in oldSlots)
+                _db.Slots.Remove(slot);
+
+            // Adds new schedule (in the form of slots)
+            foreach (Slot slot in newSlots)
+                await _db.Slots.AddAsync(slot);
+
+            _db.SaveChanges();
+        }
+
+        /// <summary>
+        /// Returns an array of Slots for a TA User
+        /// </summary>
+        public async Task<Slot[]> GetSchedule()
+        {
+            TAUser currUser = await _um.GetUserAsync(User);
+            Slot[] oldSlots = _db.Slots.Where(slot => slot.TAUserId == currUser.Id).ToArray();
+            return oldSlots;
+        }
+    }
+}
