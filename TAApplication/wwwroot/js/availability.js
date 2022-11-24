6f7d1a141e805fa9d79c5122cddb43ab82f1a001
rj-davidson <hello@rjd.app>
2022-11-24 10:11:47 -0700

    PIXI Grid begun

--- TAApplication/wwwroot/js/availability.js
+++ TAApplication/wwwroot/js/availability.js
@@ -1,7 +1,8 @@
-﻿let bg_color = 0xabcdef;
-let rect_color = 0x550000;
+﻿let bg_color = 0x000055;
+let rect_color = 0xeeee55;
+let border_color = 0xaaaaa1;
 let width = 800;
-let height = 400;
+let height = 600;
 
 var color = 0xffffff;
 var mouse_down = false;
@@ -12,6 +13,50 @@ $("#canvas_div").append(app.view);
 
 var square_1 = build_square(1);
 var square_2 = build_square(2);
+var slots = build_slots();
+var grid = build_grid();
+
+function build_grid() {
+    const numDaysWithBuffer = 6;
+    const numSlotsWithBuffer = 56;
+    const slotWidth = width / numDaysWithBuffer;
+    const slotHeight = height / numSlotsWithBuffer;
+    var grid = new PIXI.Graphics();
+    grid.x = 20;
+    grid.y = 4 * slotHeight;
+    grid.beginFill(border_color);
+    // Horizontal Lines
+    for (var i = 0; i < numSlotsWithBuffer - 6; i += 4) {
+        grid.drawRect(0, i * slotHeight, width - slotWidth, 1);
+    }
+    // Vertical Lines
+    for (var i = 0; i < numDaysWithBuffer; i++) {
+        grid.drawRect(i * slotWidth, 0, 1, height - slotHeight * 8);
+    }
+    app.stage.addChild(grid);
+    return grid;
+}
+
+function build_slots() {
+    const available_color = 0x343464;
+    const unavailable_color = 0x2f2f2f;
+    const numDaysWithBuffer = 6;
+    const numSlotsWithBuffer = 56;
+    const slotWidth = width / numDaysWithBuffer;
+    const slotHeight = height / numSlotsWithBuffer;
+    var slotGrid = new PIXI.Graphics();
+    slotGrid.x = 20;
+    slotGrid.y = 4 * slotHeight;
+    slotGrid.beginFill(available_color);
+    // Vertical Lines
+    for (var i = 0; i < 5 * 48; i++) {
+        slotGrid.drawRect(Math.floor(i / 48) * slotWidth, i % 48 * slotHeight, slotWidth, slotHeight);
+        
+    }
+    
+    app.stage.addChild(slotGrid);
+    return slotGrid;
+}
 
 function build_square(id) {
     var square = new PIXI.Graphics();
@@ -55,6 +100,10 @@ function pointer_over() {
     }
 }
 
+class Slot {
+    constructor(slotNum, available) {        this.slotNum = slotNum;        this.available = available;    }
+
+}
     // some thoughts:
     // 1) sometimes "this" is not what you think it is, if so use bind
     // 2) start simple and test, use abstraction
