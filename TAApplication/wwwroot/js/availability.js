89d480f042f1bd3ba005d853a20b60c7511b9f30
rj-davidson <hello@rjd.app>
2022-11-28 20:11:20 -0700

    Added AvailabilityTracker class and slight refactor of Slot class

--- TAApplication/wwwroot/js/availability.js
+++ TAApplication/wwwroot/js/availability.js
@@ -16,13 +16,18 @@ File Contents
 Draws availability schedule for user. Allows editing and saving availability.
 */
 
+
+
+
 class Slot extends PIXI.Graphics {
     static selected_valid = false;
     static selected_slotNum = -1;
     static selected_availability = true;
-    constructor(slotNum, available, x, y, my_width, my_height)
+    constructor(availability_tracker, slotNum, available, x, y, my_width, my_height)
     {
         super();
+
+        this.availability_tracker = availability_tracker;
 
         this.slotNum = slotNum;
         this.available = available;
@@ -54,14 +59,14 @@ class Slot extends PIXI.Graphics {
 
         this.available = !this.available;
         this.draw_slot();
-        mouse_down = true;
+        this.availability_tracker.mouse_down = true;
         console.log("available: "+this.available);
     }
 
     pointer_up() {
         console.log("pointer up");
         Slot.selected_valid = false;
-        mouse_down = false;
+        this.availability_tracker.mouse_down = false;
 
         var selected_col = Math.floor(Slot.selected_slotNum/48);
         var this_col = Math.floor(this.slotNum/48);
@@ -71,122 +76,135 @@ class Slot extends PIXI.Graphics {
 
         if(Slot.selected_slotNum < this.slotNum){
             for(var i = Slot.selected_slotNum; i <= this.slotNum; i++){
-                slots[i].available = !Slot.selected_availability;
-                slots[i].draw_slot();
+                this.availability_tracker.slots[i].available = !Slot.selected_availability;
+                this.availability_tracker.slots[i].draw_slot();
             }
         } else {
             for(var i = Slot.selected_slotNum; i >= this.slotNum; i--){
-                slots[i].available = !Slot.selected_availability;
-                slots[i].draw_slot();
+                this.availability_tracker.slots[i].available = !Slot.selected_availability;
+                this.availability_tracker.slots[i].draw_slot();
             }
         }
     }
 
     pointer_over() {
-        if(mouse_down){
+        if(this.availability_tracker.mouse_down){
             this.available = !Slot.selected_availability;
             this.draw_slot();
         }
     }
 }
 
-const bg_color = 0xe2e4fa;
-const border_color = 0xa9a9a9;
-const available_color = 0x343464;
-const unavailable_color = 0x2f2f2f;
-const width = 800;
-const height = 600;
 
-var mouse_down = false;
+class AvailabilityTracker extends PIXI.Graphics {
 
-app = new PIXI.Application({ backgroundColor: bg_color });
-app.renderer.resize(width, height);
-$("#canvas_div").append(app.view);
+    static bg_color = 0xe2e4fa;
+    static border_color = 0xa9a9a9;
+    static available_color = 0x343464;
+    static unavailable_color = 0x2f2f2f;
+    static width = 800;
+    static height = 600;
 
-var slots = build_slots();
-var grid = build_grid();
+    static mouse_down = false;
 
-load_slots();
+    constructor(){
+        super();
 
-function build_grid() {
-    const numDaysWithBuffer = 6;
-    const numSlotsWithBuffer = 56;
-    const slotWidth = width / numDaysWithBuffer;
-    const slotHeight = height / numSlotsWithBuffer;
-    var grid = new PIXI.Graphics();
-    grid.x = 20;
-    grid.y = 4 * slotHeight;
-    grid.beginFill(border_color);
-// Horizontal Lines
-    for (var i = 0; i < numSlotsWithBuffer - 6; i += 4) {
-        grid.drawRect(0, i * slotHeight, width - slotWidth, 1);
+        this.app = new PIXI.Application({ backgroundColor: AvailabilityTracker.bg_color });
+        this.app.renderer.resize(AvailabilityTracker.width, AvailabilityTracker.height);
+        $("#canvas_div").append(this.app.view);
+
+        this.slots = this.build_slots();
+        this.grid = this.build_grid();
+
+        this.load_slots();
     }
-    app.stage.addChild(grid);
-    return grid;
-}
 
-function build_slots() {
-    const numDaysWithBuffer = 6;
-    const numSlotsWithBuffer = 56;
-    const slotWidth = width / numDaysWithBuffer;
-    const slotHeight = height / numSlotsWithBuffer;
-    var offsetX = 20;
-    var offsetY = 4*slotHeight;
 
-    var xGap = 12;
-let slot_arr = []; // js representation of Slot objects
-
-// Slot Initialization
-for (let i = 0; i < 5 * 48; i++) {
-    var x = offsetX + (Math.floor(i / 48) * slotWidth);
-    var y = offsetY + (i % 48 * slotHeight);
-//        var isAvailable = slot_data[i]["isOpen"];
-
-    var new_slot = new Slot(i, true, x + xGap/2, y, slotWidth - xGap, slotHeight);
-    new_slot.draw_slot();
-    app.stage.addChild(new_slot);
-    slot_arr.push(new_slot);
-}
-
-return slot_arr;
-}
-// some thoughts:
-// 1) sometimes "this" is not what you think it is, if so use bind
-// 2) start simple and test, use abstraction
-
-function load_slots() {
-    existing_slots = [];
-
-    $.ajax({
-        url: "Availability/GetSchedule/",
-        type: "GET"
-    })
-    .done(function (data) {
-        existing_slots = data;
-        console.log(data);
-        for(var i = 0; i < 5*48; i++){
-            slots[i].available = existing_slots[i]["isOpen"];
-            slots[i].draw_slot();
-            console.log("slot data i: " + i + " " + existing_slots[i]["isOpen"]);
-
+    build_grid() {
+        const numDaysWithBuffer = 6;
+        const numSlotsWithBuffer = 56;
+        const slotWidth = AvailabilityTracker.width / numDaysWithBuffer;
+        const slotHeight = AvailabilityTracker.height / numSlotsWithBuffer;
+        var grid = new PIXI.Graphics();
+        grid.x = 20;
+        grid.y = 4 * slotHeight;
+        grid.beginFill(AvailabilityTracker.border_color);
+        // Horizontal Lines
+        for (var i = 0; i < numSlotsWithBuffer - 6; i += 4) {
+            grid.drawRect(0, i * slotHeight, AvailabilityTracker.width - slotWidth, 1);
         }
-    })
-    .fail(function (data) { console.log("Catastrophe! Slots were not recieved from Database."); });
+        this.app.stage.addChild(grid);
+        return grid;
+    }
 
-    return existing_slots;
+    build_slots() {
+        const numDaysWithBuffer = 6;
+        const numSlotsWithBuffer = 56;
+        const slotWidth = AvailabilityTracker.width / numDaysWithBuffer;
+        const slotHeight = AvailabilityTracker.height / numSlotsWithBuffer;
+        var offsetX = 20;
+        var offsetY = 4*slotHeight;
+
+        var xGap = 12;
+        let slot_arr = []; // js representation of Slot objects
+
+        // Slot Initialization
+        for (let i = 0; i < 5 * 48; i++) {
+            var x = offsetX + (Math.floor(i / 48) * slotWidth);
+            var y = offsetY + (i % 48 * slotHeight);
+    //        var isAvailable = slot_data[i]["isOpen"];
+
+        var new_slot = new Slot(this, i, true, x + xGap/2, y, slotWidth - xGap, slotHeight);
+        new_slot.draw_slot();
+        this.app.stage.addChild(new_slot);
+        slot_arr.push(new_slot);
+    }
+
+    return slot_arr;
+    }
+    // some thoughts:
+    // 1) sometimes "this" is not what you think it is, if so use bind
+    // 2) start simple and test, use abstraction
+
+    load_slots() {
+        let existing_slots = [];
+        console.log("1 ", this);
+
+        $.ajax({
+            url: "Availability/GetSchedule/",
+            type: "GET"
+        })
+        .done( (function (data) {
+            existing_slots = data;
+            console.log(data);
+            console.log("2 ",this)
+            for(var i = 0; i < 5*48; i++){
+                this.slots[i].available = existing_slots[i]["isOpen"];
+                this.slots[i].draw_slot();
+                console.log("slot data i: " + i + " " + existing_slots[i]["isOpen"]);
+            }
+        }).bind(this) )
+        .fail(function (data) { console.log("Catastrophe! Slots were not recieved from Database."); });
+
+        return existing_slots;
+    }
+
+    upload_slots(){
+        var data;
+        $.ajax({
+            url: "Availability/SetSchedule/",
+            type: "POST",
+            data: data
+        })
+        .done(function (data) {
+            console.log("successfully saved, probably");
+        })
+        .fail(function (data) {
+            console.log("so sad, not saved");
+        })
+    }
+
 }
 
-function upload_slots(){
-    var data;
-    $.ajax({
-        url: "Availability/SetSchedule/",
-        type: "POST",
-        data: data
-    })
-    .done(function (data) {
-        console.log("successfully saved, probably");
-    })
-    .fail(function (data) {
-        console.log("so sad, not saved");
-    })
-}
+const availability_tracker = new AvailabilityTracker();
